6895016d65cdad66fcb8fdc960ade28267ddb078
Lcasmendes <lucassm@estudante.ufscar.br>
2024-08-15 13:07:13 -0300

    Pagina home listando medicos com pesquisa e inicio da exclusão

--- src/main/java/br/ufscar/dc/consultas/controller/MedicoController.java
+++ src/main/java/br/ufscar/dc/consultas/controller/MedicoController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @Controller
@@ -16,6 +19,35 @@ public class MedicoController {
 
     @Autowired
     private MedicoDAO medicoDAO;  
+    
+
+    // Método GET para listar todos os médicos ou filtrar por especialidade
+    @GetMapping("/home")
+    public String listarMedicosHome(Model model, @RequestParam(value = "especialidade", required = false) String especialidade) {
+        List<Medico> medicos;
+        if (especialidade == null || especialidade.isEmpty()) {
+            // Se nenhuma especialidade for fornecida, lista todos os médicos
+            medicos = medicoDAO.findAll();
+        } else {
+            // Se uma especialidade for fornecida, filtra os médicos por essa especialidade
+            medicos = medicoDAO.buscarPorEspecialidade(especialidade);
+        }
+        // Adiciona a lista de médicos ao modelo
+        model.addAttribute("medicos", medicos);
+        // Retorna o nome da view a ser renderizada
+        return "medicos_home";
+    }
+
+    // Método POST para buscar médicos por especialidade
+    @PostMapping("/home")
+    public String buscaEspecialidade(@RequestParam("especialidade") String especialidade, Model model) {
+        // Filtra os médicos pela especialidade fornecida
+        List<Medico> medicos = medicoDAO.buscarPorEspecialidade(especialidade);
+        // Adiciona a lista de médicos ao modelo
+        model.addAttribute("medicos", medicos);
+        // Retorna a view com os médicos filtrados
+        return "medicos_home";
+    }
 
     // Mapeia requisições HTTP GET para "/medicos" para listar todos os médicos
     @GetMapping("/CRUD")
@@ -45,5 +77,14 @@ public class MedicoController {
         // Redireciona para a lista de médicos após a adição
         return "redirect:/medicos/CRUD";
     }
+    
+    // Método POST para excluir um médico pelo seu ID
+    @PostMapping("/excluir")
+    public String excluirMedico(@RequestParam("id") Long id) {
+        medicoDAO.deleteById(id);
+        return "redirect:/medicos/CRUD";
+    }
+    
+    
 
 }
